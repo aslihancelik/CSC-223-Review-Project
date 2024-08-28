19dc28ef61f729d8c3f398215bf5a360f9f81d63
aslihancelik <aslihancelik27@gmail.com>
2024-08-28 12:25:04 -0400

    Code complete.

---
+++ PointType.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+template <class T>
+class PointType
+{
+private:
+	T x;
+	T y;
+public:
+
+	//Default Constructor
+	PointType() { x = 0, y = 0, cout << fixed << setprecision(1) << showpoint;  }
+	//Constructor with Parameters
+	PointType(T Ux, T Uy) : x(Ux), y(Uy) { cout << fixed << setprecision(1) << showpoint; }   
+
+	//Setters
+	void setX(T Ux) { x = Ux; }
+	void set(T Uy) { y = Uy; }
+	void setPoint(T Ux, T Uy) {
+		x = Ux;
+		y = Uy;
+	}
+
+	//Getters
+	T getX() { return x; }
+	T getY() { return y; }
+	void getPoint(T& Ux, T& Uy) {
+		Ux = x;
+		Uy = y;
+	}
+
+	double operator -(PointType<T> op2) {
+		double distance = sqrt(pow(x - op2.x, 2) + pow(y - op2.y, 2));
+		return distance;
+
+	}
+
+	void print()
+	{
+		cout << "(" << x << "," << y << ")" << endl;
+	}
+
+	
+};
+
