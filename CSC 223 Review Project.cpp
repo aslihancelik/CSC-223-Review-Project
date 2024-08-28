e8653205512ddc9f599c0a42d148fa349e82aae6
aac12526 <aac12526@HA-STEMCART-53>
2024-08-28 11:15:05 -0400

    Add project files.

---
+++ CSC 223 Review Project.cpp
@@ -0,0 +1,11 @@
+// CSC 223 Review Project.cpp : This file contains the code for yhr Review 
+// which will be three branches, PointType Class, CircleType Class, CylinderType Class
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "CSC 223 Review Project\n";
+}
+
