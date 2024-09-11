71e7dff697aacfd54ca42387e00302428fa4df55
aslihancelik <aslihancelik27@gmail.com>
2024-09-11 18:44:59 -0400

    Finalized changes.

--- CSC 223 Review Project.cpp
+++ CSC 223 Review Project.cpp
@@ -1,7 +1,10 @@
 /*
-CSC 223 Review Project.cpp : This program tests the CircleType class by creating circle objects with both integer and
-floating-point coordinates. It demonstrates the use of constructors, setters, and getters, and verifies the calculation
-and printing of the circle's radius, circumference, and area.
+*CSC 223 Review Project.cpp : This program tests the CircleType class by creating circle objects with both integer and
+*floating-point coordinates. It demonstrates the use of constructors, setters, and getters, and verifies the calculation
+*and printing of the circle's radius, circumference, and area.
+*
+* 
+* Programmer:Aslihan Celik
 */
 
 #include <iostream>
@@ -12,7 +15,7 @@ using namespace std;
 int main()
 {
    
-    cout << "Testing Default Constructor" << endl;
+    cout << "Testing Default Constructor for Circle1" << endl;
     CircleType<int> Cir1;
     cout << "Center point: ";
     Cir1.printCenterPoint();
@@ -20,7 +23,7 @@ int main()
     Cir1.printCircumPoint();
     cout << endl;
 
-    cout << "Test constructor with parameters" << endl;
+    cout << "Test constructor with parameters for Circle2" << endl;
     CircleType<double> Cir2(5.5, 7.2, -6.4, 3.3);
     cout << "Center point: ";
     Cir2.printCenterPoint();
@@ -28,7 +31,7 @@ int main()
     Cir2.printCircumPoint();
     cout << endl;
 
-    cout << "Testing setCenterPoint, setCircumPoint" << endl;
+    cout << "Testing setCenterPoint, setCircumPoint for Circle1" << endl;
     Cir1.setCenterPoint(3, 7);
     Cir1.setCircumPoint(6, -2);
     cout << "Center Point: ";
@@ -37,13 +40,15 @@ int main()
     Cir1.printCircumPoint();
     cout << endl;
 
-    cout << "Testing getCenterPoint, getCircumPoint" << endl;
+    cout << "Testing getCenterPoint, getCircumPoint with Circle2" << endl;
     double x1, y1, x2, y2;
     Cir2.getCenterPoint(x1, y1);
     Cir2.getCircumPoint(x2, y2);
     cout << "x1 = " << x1 << " y1 = " << y1 << " x2 = " << x2 << " y2 = " << y2 << endl;
+    cout << endl;
 
-    cout << "Testing operations: " << endl;
+    cout << "Testing the calculations for radius, circumference and" << endl;
+    cout << "area for Circle2 " << endl;
     Cir2.print();
     cout << endl;
     
